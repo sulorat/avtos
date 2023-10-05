e44d06db34b103d5d207370829e389c8e3d5f855
sulorat <135011124+sulorat@users.noreply.github.com>
2023-10-06 01:09:33 +0300

    last version

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace oop{ 
+internal class Program
+{
+        static void Main(string[] args)
+        {
+            avto car = new avto();
+            car.info(car);
+            car.infoOut();
+            string y_n = "";
+            if (car.will_he_get_there() == true)
+            {
+                Console.WriteLine("Вы не сможете доехать до назначенного местаю\n Хотите заправиться(y/n)? ");
+                y_n = Console.ReadLine();
+            }
+
+            if (y_n == "y")
+            {
+                car.zapravka();
+            }
+            if (y_n == "n")
+                Console.WriteLine("Вы не сможете доехать");
+            car.drive();
+
+
+        }
+
+    }
+}
